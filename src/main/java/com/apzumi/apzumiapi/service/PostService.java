f3965b232ebedf50b1bfd69d560e7f8d25a1e7cf
piotr <piotr.lisowski@outlook.com>
2021-03-17 17:15:10 +0100

    added error handling for API wrong URL, now it returns server response code

--- src/main/java/com/apzumi/apzumiapi/service/PostService.java
+++ src/main/java/com/apzumi/apzumiapi/service/PostService.java
@@ -6,10 +6,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -21,11 +24,16 @@ public class PostService {
 
     @Scheduled(fixedRate = 43200000)
     public void downloadPosts() {
-        String url = "https://jsonplaceholder.typicode.com/posts";
-        Post[] posts = restTemplate.getForObject(url, Post[].class);
-        List<Post> downloadedPosts = Arrays.asList(posts);
-        saveAll(downloadedPosts);
-        System.out.println("Posts saved to database!");
+        String url = "https://jsonplaceholder.typicode.com/postss";
+        try {
+            Post[] posts = restTemplate.getForObject(url, Post[].class);
+            List<Post> downloadedPosts = Optional.ofNullable(posts).map(Arrays::asList).orElseGet(ArrayList::new);
+            saveAll(downloadedPosts);
+            System.out.println("Posts saved to database!");
+        } catch (HttpStatusCodeException exception) {
+            int statusCode = exception.getStatusCode().value();
+            System.out.println("JSON API URL returns: " + statusCode);
+        }
     }
 
     public Iterable<Post> listAll() {
