5cc4238bfaf95d96d4d06911c26691b3cd49c532
piotr <piotr.lisowski@outlook.com>
2021-04-14 21:24:40 +0200

    removing some unused imports, and app is still working! today was a good day

--- src/main/java/com/apzumi/apzumiapi/repository/PostRepository.java
+++ src/main/java/com/apzumi/apzumiapi/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package com.apzumi.apzumiapi.repository;
 
 import com.apzumi.apzumiapi.domain.Post;
-import com.apzumi.apzumiapi.domain.PostDTO;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
