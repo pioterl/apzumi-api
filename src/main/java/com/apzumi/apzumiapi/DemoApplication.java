a41d166257ec6ccc4cb210ff3fdc7dadf6bdb7f1
piotr <piotr.lisowski@outlook.com>
2021-04-14 21:21:51 +0200

    removing some default and unnecessary swagger endpoints: basic-error-controller, operation-handler, web-mvc-links-handler

--- src/main/java/com/apzumi/apzumiapi/DemoApplication.java
+++ src/main/java/com/apzumi/apzumiapi/DemoApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.function.Predicate;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -16,6 +22,14 @@ public class DemoApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.apzumi.apzumiapi"))
+                .build();
     }
 
 
